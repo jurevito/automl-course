7231659034142a99ab0e4d35162b987963e5b63b
jurevito <jure.vito@gmail.com>
2022-11-12 21:00:06 +0200

    Implement hyperopt

--- project1/util.py
+++ project1/util.py
@@ -2,10 +2,12 @@ import numpy as np
 import scipy as scp
 import pandas as pd
 import seaborn as sn
-from sklearn import preprocessing
+from matplotlib import pyplot
+from hyperopt import fmin, hp, tpe, STATUS_OK
+
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split, KFold
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import KFold
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -36,7 +38,7 @@ category_columns = ['Sex', 'ChestPainType', 'RestingECG', 'ExerciseAngina', 'ST_
 
 def preprocess(df: pd.DataFrame):
 
-    le = preprocessing.LabelEncoder()
+    le = LabelEncoder()
     for column in category_columns:
         df[column] = le.fit_transform(df[column])
 
@@ -51,7 +53,7 @@ def find_baseline(df: pd.DataFrame) -> dict[str, float]:
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
     results = {}
 
-    for name,  classifier in classifiers.items():
+    for name,  model in classifiers.items():
         scores = []
 
         for train_index, test_index in kf.split(X):
@@ -61,7 +63,7 @@ def find_baseline(df: pd.DataFrame) -> dict[str, float]:
             scaler.fit_transform(X_train)
             scaler.transform(X_test)
 
-            model = classifier.fit(X=X_train, y=y_train)
+            model.fit(X=X_train, y=y_train)
             y_pred = model.predict(X_test)
 
             score = accuracy_score(y_test, y_pred)
@@ -71,6 +73,41 @@ def find_baseline(df: pd.DataFrame) -> dict[str, float]:
 
     return results
 
+def create_objective(classifier, df: pd.DataFrame, scale_values: bool = False):
+
+    X = df.drop('HeartDisease', axis=1).values
+    y = df['HeartDisease'].values
+
+    def objective(search_space):
+
+        try:
+            model = classifier(**search_space, random_state=42)
+        except:
+            model = classifier(**search_space)
+
+        scaler = StandardScaler()
+        kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+        scores = []
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+
+            if scale_values:
+                scaler.fit_transform(X_train)
+                scaler.transform(X_test)
+
+            model.fit(X_train, y_train)
+
+            y_pred = model.predict(X_test)
+            score = accuracy_score(y_test, y_pred)
+            scores.append(score)
+
+        avg_score = sum(scores) / len(scores)
+
+        return {'loss': 1 - avg_score, 'status': STATUS_OK}
+
+    return objective
 
 if __name__ == '__main__':
     df = pd.read_csv("./heart_failure.csv")
